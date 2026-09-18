395f0607a04c7bb69b33f095c63ae632fcc40795
kanirudhsaxena-code <k.anirudhsaxena@gmail.com>
2026-09-18 10:37:11 +0530

    Fix Upstox quote request headers (#14)
    * Add JSON content type to Upstox market requests
    
    * Add JSON content type to Upstox research requests
    
    * Trigger quote header validation rerun

--- src/upstox_research.py
+++ src/upstox_research.py
@@ -62,6 +62,7 @@ class UpstoxReadOnlyResearchProvider:
                 url,
                 headers={
                     "Accept": "application/json",
+                    "Content-Type": "application/json",
                     "Authorization": "Bearer " + self._token,
                     "User-Agent": "EDGE-Stocks-Readonly-Research/1.0",
                 },
